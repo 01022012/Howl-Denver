db8ce078f99ac0fc2a6dc8c50df524533e76631e
Dmitry Kolobov <dmkolobov@gmail.com>
2011-11-29 15:51:42 -0700

    heroku commit

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,6 @@
 class ArtistsController < ApplicationController
   def index
-    @artists = Artist.all
+    @artists = Artist.order(:name)
   end
   
   def show
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,6 @@
 class VenuesController < ApplicationController
   def index
-    @venues = Venue.all
+    @venues = Venue.order(:venue_type)
   end
   
   def show
